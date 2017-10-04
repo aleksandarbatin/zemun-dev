193b1135f0196c8595d2f7019ae6f89704b9e10e
Aleksandar Batin <aleksandarbatin@gmail.com>
2017-10-04 16:28:51 +0200

    image loader problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,69 @@
-const {resolve} = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
 
 module.exports = {
-    context: resolve(__dirname, 'src'),
-    entry: [
-        './scripts/main.js'
-        // the entry point of our app
-    ],
+    entry: './src/scripts/main.js',
     output: {
-        path: resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
         // publicPath: '/dist'
     },
-    devtool: 'source-map',
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
-                use: ['babel-loader'],
-                exclude: /node_modules/
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
             },
             {
                 test: /\.scss$/,
-                use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-                        use: [
-                            {
-                                loader: "css-loader" // translates CSS into CommonJS
-                            },
-                            {
-                                loader: "sass-loader" // compiles Sass to CSS
-                            }
-                        ],
-                        fallback: "style-loader" // used when css not extracted
-                    }
-                ))
-            },
-            {
-                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-                use: 'url-loader'
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
             },
             {
                 test: /\.html$/,
-                use: ["html-loader"]
+                use: ['html-loader']
             },
             {
                 test: /\.(jpg|png)$/,
                 use: [
                     {
-                        loader: "file-loader",
+                        loader: 'file-loader',
                         options: {
-                            name: "[name].[ext]",
-                            outputPath: "images/",
-                            publicPath: "images/"
+                            name: '[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: 'images/'
                         }
                     }
                 ]
+            },
+            {
+                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+                use: 'url-loader'
             }
         ]
     },
     plugins: [
-        new webpack.NamedModulesPlugin(),
-        // prints more readable module names in the browser console on HMR updates
-
-        new ExtractTextPlugin({filename: 'main.css', allChunks: true}),
+        extractPlugin,
         new HtmlWebpackPlugin({
-            template: "index.html"
+            template: 'src/index.html'
         })
-    ],
+        // new CleanWebpackPlugin(['dist'])
+    ]
 };
