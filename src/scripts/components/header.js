42f4edb710da9e0b52a6d617bc422574e18b613e
Aleksandar Batin <aleksandarbatin@gmail.com>
2017-11-13 14:41:15 +0100

    search expand

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -8,24 +8,41 @@ import React, { Component } from 'react';
 
 
 class Header extends Component {
+	constructor(props){
+        super(props);
+        this.state = { expand: false };
+        this.expandSearch = this.expandSearch.bind(this);
+        // this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
+    expandSearch() {
+        // if (!this.state.expand) {
+        //     // attach/remove event handler
+        //     document.addEventListener('click', this.handleOutsideClick, false);
+        // } else {
+        //     document.removeEventListener('click', this.handleOutsideClick, false);
+        // }
+        this.setState(prevState => ({
+           expand: !prevState.expand,
+        })); //Flips true/false
+    }
 	render() {
 		return (
 			<header id="header">
 				<h1><a href="/">Zemun</a></h1>
 				<nav className="links">
 					<ul>
-						<li><a href="/">Lorem</a></li>
-						<li><a href="/">Ipsum</a></li>
-						<li><a href="/">Feugiat</a></li>
-						<li><a href="/">Tempus</a></li>
-						<li><a href="/">Adipiscing</a></li>
+						<li><a href="/">Istorija</a></li>
+						<li><a href="/">Znamenitosti</a></li>
+						<li><a href="/">Poznati Zemunci</a></li>
+						<li><a href="/">Price</a></li>
+						<li><a href="/">Blog</a></li>
 					</ul>
 				</nav>
 				<nav className="main">
 					<ul>
 						<li className="search">
-							<a className="fa-search" href="#search">Search</a>
-							<form id="search" method="get" action="#">
+							<a className="fa-search" onClick={this.expandSearch}>Search</a>
+							<form id="search" method="get" action="#" className={(this.state.expand ? "visible " : "")}>
 								<input type="text" name="query" placeholder="Search" />
 							</form>
 						</li>
